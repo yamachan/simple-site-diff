1ef4189574c5fd8eeb5ea78f3897c05befd04c27
yamachan <z@rinco.jp>
2019-06-16 17:11:55 +0900

    Update README

--- transform.js
+++ transform.js
@@ -1,10 +1,4 @@
 function simple_site_diff_transform(_url) {
 	let url = (_url||'');
-	if (url.indexOf('developer.ibm.com/cn') >= 0) { // Sample for IBM Developer China site
-		return url.replace('developer.ibm.com/cn', 'ibmdev1.rtp.raleigh.ibm.com/zh');
-	} else if (url.indexOf('developer.ibm.com/') >= 0) { // Sample for some IBM Developer sites
-		return url.replace('developer.ibm.com/', 'ibmdev1.rtp.raleigh.ibm.com/');
-	} else {
-		return url.replace('www.example.com/', 'wwwtest.example.com/');
-	}
+	return url.replace('www.example.com/', 'wwwtest.example.com/');
 }
